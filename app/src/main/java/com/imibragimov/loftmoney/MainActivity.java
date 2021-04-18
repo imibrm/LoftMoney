03808c2b8130c0211100cbedccbd960c37ce31f6
imibrm <imibrm@mail.ru>
2021-04-18 14:41:31 +0300

    Add items

--- app/src/main/java/com/imibragimov/loftmoney/MainActivity.java
+++ app/src/main/java/com/imibragimov/loftmoney/MainActivity.java
@@ -6,6 +6,34 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public MainActivity() {
+        super();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
